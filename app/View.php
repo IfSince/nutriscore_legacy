1a2f91dffe6322b80c4387fdca05baa383931dbf
IfSince <leonlaade@gmx.de>
2023-02-24 21:39:24 +0100

    smool clean up

--- app/View.php
+++ app/View.php
@@ -7,9 +7,9 @@ class View {
     public function render(string $view, array $data = [], int $statusCode = 200): void {
         extract($data);
 
-        require_once __DIR__ . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . 'renders.php';
+        require_once __DIR__ . str_replace('/', DIRECTORY_SEPARATOR, "/Views/helpers/renders.php");
         require_once __DIR__ . str_replace('/', DIRECTORY_SEPARATOR, "/Views/$view.php");
-        require_once __DIR__ . DIRECTORY_SEPARATOR . 'Views' . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'footer.php';
+        require_once __DIR__ . str_replace('/', DIRECTORY_SEPARATOR, "/Views/partials/footer.php");
 
         http_response_code($statusCode);
         exit();
