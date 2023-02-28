da5d00df11d23b918ec2b059991d49711098cd99
IfSince <leonlaade@gmx.de>
2023-02-28 21:05:20 +0100

    fix const 2

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 namespace NutriScore;
 
 // Directories
-define('APP_ROOT_DIR', __DIR__);
-define('APP_PUBLIC_DIR', APP_ROOT_DIR . DIRECTORY_SEPARATOR. 'public' . DIRECTORY_SEPARATOR);
+define('APP_ROOT_DIR', __DIR__ . DIRECTORY_SEPARATOR. 'public');
+define('APP_PUBLIC_DIR', APP_ROOT_DIR . DIRECTORY_SEPARATOR);
 define('APP_UPLOADS_DIR', APP_PUBLIC_DIR . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'images');
